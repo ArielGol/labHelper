c71449f0a5808f39e374572ecc3123743e08fba1
Ariel Golubickas <ariel00golubickas@gmail.com>
2026-08-12 22:49:41 -0300

    base de datos terminada

---
+++ src/main/resources/database/init_db.sql
@@ -0,0 +1,10 @@
+INSERT INTO TIPOS_DE_CLIENTES (ID,NOMBRE) values (1,'Interno');
+INSERT INTO TIPOS_DE_CLIENTES (ID,NOMBRE) values (2,'Externo');
+
+INSERT INTO ESTADOS (ID,NOMBRE) values (1,'Recibido');
+INSERT INTO ESTADOS (ID,NOMBRE) values (2,'En analisis');
+INSERT INTO ESTADOS (ID,NOMBRE) values (3,'Por validar');
+INSERT INTO ESTADOS (ID,NOMBRE) values (4,'Completado');
+INSERT INTO ESTADOS (ID,NOMBRE) values (5,'Aprobado');
+INSERT INTO ESTADOS (ID,NOMBRE) values (6,'Rechazado');
+
